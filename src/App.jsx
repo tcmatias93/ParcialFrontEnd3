3b3ac262855f7833c1f351d122ffe979be06ceda
Matias Torres Contreras <matiastorrescontreras@MacBook-Air-de-Matias.local>
2024-11-06 18:53:06 -0600

    Cambios para le entrega del parcial 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,50 @@
+import { useState } from "react";
 import "./App.css";
 import Card from "./Components/Card";
 
 function App() {
+  const [libro, setLibro] = useState({ titulo: "", autor: "" });
+  const [error, setError] = useState(false);
+  const [show, setShow] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (
+      libro.titulo.length > 3 &&
+      !/^ /.test(libro.titulo) &&
+      libro.autor.length >= 6
+    ) {
+      setShow(true);
+      setError(false);
+    } else {
+      setError(true);
+    }
+  };
+
   return (
     <>
-      <h1>Carga de estudiantes</h1>
-      <form></form>
-      <Card />
+      <h1>Carga tu libro favorito</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Ingrese el nombre de un libro: </label>
+        <input
+          type="text"
+          onChange={(e) => setLibro({ ...libro, titulo: e.target.value })}
+        />
+        <label>Ingresa el autor del libro:</label>
+        <input
+          type="text"
+          onChange={(e) => setLibro({ ...libro, autor: e.target.value })}
+        />
+        <button>Enviar</button>
+
+        {error ? (
+          <h4 style={{ color: "red" }}>
+            Por favor chequea que la información sea correcta
+          </h4>
+        ) : null}
+      </form>
+
+      {show ? <Card libro={libro} /> : null}
     </>
   );
 }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,5 +1,16 @@
-const Card = () => {
-  return <div>Hola, soy una tarjeta</div>;
+import CardStyles from "../styles/Card.module.css";
+
+const Card = ({ libro }) => {
+  return (
+    <div className={CardStyles.libroInfo}>
+      <h3>El libro que te gusta se llama:</h3>
+      <p>
+        <strong>{libro.titulo}</strong>
+      </p>
+      <h3>y es de:</h3>
+      <p>{libro.autor}</p>
+    </div>
+  );
 };
 
 export default Card;
